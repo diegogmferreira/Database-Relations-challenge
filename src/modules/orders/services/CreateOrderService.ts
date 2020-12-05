1a8d7e91caf83bed9edf7e999f82b4bde9dca9c3
Diego Galvão <diego@Diegos-MacBook-Pro.local>
2020-12-05 13:34:11 -0300

    Challenge complete

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,68 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
     // TODO
+    const customerExists = await this.customersRepository.findById(customer_id);
+
+    if (!customerExists) {
+      throw new AppError('Customer does not exists')
+    }
+
+    const productsExists = await this.productsRepository.findAllById(products);
+
+    if (!productsExists.length) {
+      throw new AppError('Products does not exists')
+    }
+
+    const productsExistsIds = productsExists.map(product => product.id);
+    const checkInexistentProducts = products.filter(
+      product => !productsExistsIds.includes(product.id)
+    )
+
+    if (checkInexistentProducts.length) {
+      throw new AppError(`Could not find product ${checkInexistentProducts[0].id}`)
+    }
+
+    const findProductsWIthNoQuantityAvailable = products.filter(
+      product => productsExists.filter(p => p.id === product.id)[0].quantity < product.quantity
+    )
+
+    if (findProductsWIthNoQuantityAvailable.length) {
+      throw new AppError(`There is no ${findProductsWIthNoQuantityAvailable[0].quantity} available for ${findProductsWIthNoQuantityAvailable[0].id}`);
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: productsExists.filter(p => p.id === product.id)[0].price
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: customerExists,
+      products: serializedProducts
+    });
+
+    const { order_products } = order;  
+
+    const orderedProductsQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity: productsExists.filter(p => p.id === product.product_id)[0].quantity - product.quantity
+    }));
+
+    await this.productsRepository.updateQuantity(orderedProductsQuantity);
+
+    return order;
   }
 }
 
--- src/modules/products/services/CreateProductService.ts
+++ src/modules/products/services/CreateProductService.ts
@@ -13,10 +13,26 @@ interface IRequest {
 
 @injectable()
 class CreateProductService {
-  constructor(private productsRepository: IProductsRepository) {}
+  constructor(
+    @inject('ProductsRepository')
+    private productsRepository: IProductsRepository
+  ) {}
 
   public async execute({ name, price, quantity }: IRequest): Promise<Product> {
     // TODO
+    const productsExistis = await this.productsRepository.findByName(name);
+
+    if (productsExistis) {
+      throw new AppError('Products already exists');
+    }
+
+    const product = await this.productsRepository.create({
+      name, 
+      price,
+      quantity
+    });
+
+    return product;
   }
 }
 
